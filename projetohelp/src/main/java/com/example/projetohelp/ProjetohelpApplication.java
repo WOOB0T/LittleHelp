e6a6148623ab0d672342c515437f80c8d7686f29
Felipe Renan <felipeferreira3146@gmail.com>
2024-03-22 21:15:45 -0300

    Projeto Spring Boot

--- projetohelp/src/main/java/com/example/projetohelp/ProjetohelpApplication.java
+++ projetohelp/src/main/java/com/example/projetohelp/ProjetohelpApplication.java
@@ -1,13 +1,13 @@
-package com.example.springboot;
+package com.example.projetohelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class ProjetohelpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(ProjetohelpApplication.class, args);
 	}
 
 }
